2e9d7961e5e4d8203d8a2d1e1c1eeaf9a366b8e1
Sharath George M <sharathgeorgem@gmail.com>
2016-08-10 21:17:22 +0530

    Symbolic constants

--- 1-5.c
+++ 1-5.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#define lower 0
+#define upper 300
+#define step 20
 int main() {
 	int celsius;
 	float fahr;
-	for(celsius = 300; celsius >= 0 ; celsius -= 20)
+	for(celsius = upper; celsius >= lower ; celsius -= step)
 		printf("%3d\t%3.2f\n", celsius, fahr = (float)celsius * 9.0 / 5.0 + 30);
 	return 0;
 }
