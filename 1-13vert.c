4e0a0f522194dfab9eb60feeab2458dee945e8f1
Sharath George M <sharathgeorgem@gmail.com>
2016-08-11 23:57:28 +0530

    Histogram vertical

---
+++ 1-13vert.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#define MAX_WORD_LENGTH 50
+#define IN 0
+#define OUT 1
+int main() {
+	char c;
+	int i, j, num_word_length[11], num_char = 0, state;
+	for( i = 0; i < 11; i++ )
+		num_word_length[i] = 0;
+	while((c = getchar()) != EOF) {
+		if(c == ' ' || c == '\n' || c == '\t')
+			state = OUT;
+		else if( c >= 'a' && c <= 'z' || c >= '0' && c <= '9') {
+			state = IN;
+			++num_char;
+		}
+		if(state) {
+			++num_word_length[num_char];
+			num_char = 0;
+		}
+	}
+	/*for( i = 1; i < 11; i++) {
+		printf("%d\t",i);
+		for( j = 0; j < num_word_length[i]; j++) {
+			printf("X");
+		}
+		printf("\n");
+	}*/
+	for( i = 10; i > 0; i--) {
+		for( j = 1; j < 11; j++) {
+			if(i > num_word_length[j])
+				printf("X");
+			else
+				printf("-");
+		}
+		printf("\n");
+	}
+	for( j = 1; j < 11; j++)
+		printf("%d", j);
+	printf("\n");
+	return 0;
+}
