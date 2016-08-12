a3e92031be827e04630a949665aec3266331b1ba
Sharath George M <sharathgeorgem@gmail.com>
2016-08-12 23:08:17 +0530

    Functions count

---
+++ 1-16.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+
+	max = 0;
+	while()
+
+return 0;
+}
